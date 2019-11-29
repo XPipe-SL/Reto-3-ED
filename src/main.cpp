e93473e9e7942bb7f662409fd03e390c605b78e8
Alejandro-Villa <awewanwo@disroot.org>
2019-11-29 18:56:29 +0100

    Main v1.0

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,22 @@ using namespace std;
 
 int main(){
 
+	cout << "Implementación de Cola (std::queue) mediante una pila (std::stack)" << endl;	
+
+
+	cout << "Construimos una cola con enteros del 1 al 10" << endl;
+	cola<int> micola;
+	for (unsigned i=1; i <= 10; ++i)
+		micola.push(i);
+	
+	cola<int> aux(micola);
+	cout << "Cola: {";
+	for (unsigned i=0; i < micola.size(); ++i) {
+		cout << aux.front() << ", ";
+		aux.pop();
+	}
+	cout << "}" << endl;
+
 	return 0;
 
 }
